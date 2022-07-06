d7c96a174e21082d9390d936bd694560887be159
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-07-06 15:39:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/movies_club_2020')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/cinema_collections')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
