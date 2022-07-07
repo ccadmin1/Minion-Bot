4afd7cf37961da52617015c60eda4382f52da7c0
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-07-07 14:06:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -724,7 +724,7 @@ async def auto_filter(client, msg, spoll=False):
         btn.insert(0,
             [InlineKeyboardButton(f"🎪 {search} 🎪",callback_data="neosub")]
         )             
-   reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
